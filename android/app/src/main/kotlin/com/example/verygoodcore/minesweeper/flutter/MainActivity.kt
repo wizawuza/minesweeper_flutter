cda1929236a02bb9560d241c84e5ec5e8c5d3840
Alam Tamboli <alamgirtamboli@gmail.com>
2025-01-10 10:50:23 -0500

    First commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/minesweeper/flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.minesweeper_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:minesweeper_flutter/app/app.dart';
+import 'package:minesweeper_flutter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
